abb2a740fbb0a093e62549ad4879df1ecf8111ad
jakobr0cky <jakob.rcknbr@gmail.com>
2024-02-08 16:06:44 +0100

    test url

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// const app = require('express')();
-const http = require('http').createServer();
+const app = require('express')();
+const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
       origin: "http://localhost:4200",
       methods: ["GET", "POST"]
-    }
+    }   
   });
 
 http.on('connection',() => {
