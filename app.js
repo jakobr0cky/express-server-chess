745e7fb3fc09dd8252618e642672932f7f7ed436
jakobr0cky <jakob.rcknbr@gmail.com>
2024-02-08 16:31:41 +0100

    correct url for github page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
-      origin: "http://localhost:4200",
+      origin: "https://jakobr0cky.github.io/angular-silentchess",
       methods: ["GET", "POST"]
     }   
   });
