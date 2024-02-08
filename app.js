0378257581402c942555bb11f31430693ae5041d
jakobr0cky <jakob.rcknbr@gmail.com>
2024-02-08 04:45:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+      origin: "https://jakobr0cky.github.io/angular-silentchess/",
+      methods: ["GET", "POST"]
+    }
+  });
+
+io.on("connection", socket => {
+    console.log(`Socket ${socket.id} connected to server socket`);
+    var game;
+
+    socket.on('joinRoom',(gameId) => {
+        socket.join(gameId);
+        io.emit('joined',io.sockets.adapter.rooms.get(gameId).size);
+        socket.emit('setPlayer',io.sockets.adapter.rooms.get(gameId).size);
+
+        game = gameId;
+    })
+
+    socket.on('checkJoin',(gameId) => {
+        
+    })
+
+    socket.on('sendMove',(move) => {
+        socket.to(game).emit('receiveMove',move);
+    });
+
+    socket.on('sendBlindMode',(blindMode) => {
+        socket.to(game).emit('receiveBlindMode',blindMode);
+    });
+});
+
+http.listen(4444, () => {
+    console.log('Listening on port 4444');
+});
